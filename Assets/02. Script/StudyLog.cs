492433ffdf01c6fd7ef9572a06f3bb110d85bd4f
Yoon!verse <xaesoo30@gmail.com>
2025-05-20 11:57:28 +0900

    StudyLog.cs

--- Assets/02. Script/StudyLog.cs
+++ Assets/02. Script/StudyLog.cs
@@ -5,12 +5,12 @@ public class StudyLog : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log(this.name);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Debug.Log(this.name);
     }
 }
